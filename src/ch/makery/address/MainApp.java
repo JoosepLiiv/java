e3aee994eb187981d36d21340837d82c1fc9f68d
Joosep <joosepliiv97@gmail.com>
2017-11-24 01:30:10 +0200

    loodud PersonOverviewController

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -25,6 +25,35 @@ public class MainApp extends Application {
     }
 
     /**
+     * The data as an observable list of Persons.
+     */
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+    /**
+     * Returns the data as an observable list of Persons.
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+    /**
      * Initializes the root layout.
      */
     public void initRootLayout() {
